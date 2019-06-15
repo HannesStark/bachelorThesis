d79eaac98dd5e03f2befe72a9570e833eb064ae6
Hannes <hannes.staerk@gmail.com>
2019-06-15 15:03:28 +0200

    add new CVAE in cvae that will be run on geoinf notebook. add tensorfow MNIST cvae

--- testScript.py
+++ testScript.py
@@ -1,17 +1,2 @@
-from tifffile import imread
-import numpy as np
-import os
+import tensorflow as tf
 
-dirs = os.listdir("Track1-Truth")
-list1 = list()
-maxnumasdf = 0
-location=""
-for i in dirs:
-    pic = imread("Track1-Truth\\" + i)
-    newmaxas = np.max(pic)
-    print(type(maxnumasdf))
-    if (newmaxas > maxnumasdf):
-        maxnumasdf = newmaxas
-        location = i
-    print(maxnumasdf)
-    print(location)
