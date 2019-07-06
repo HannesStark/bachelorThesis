19b3617dcafa5db3f6fbc6cd4970db9a44445602
Hannes <hannes.staerk@gmail.com>
2019-07-06 17:53:33 +0200

    only changes in testscript

--- testScript.py
+++ testScript.py
@@ -1,4 +1,32 @@
 import tensorflow as tf
-import time
+import keras
+import IPython
+model = tf.keras.Sequential(
+            [
+                tf.keras.layers.Conv2D(
+                    filters=32, kernel_size=3, strides=(2, 2), activation='relu'),
+                tf.keras.layers.Conv2D(
+                    filters=64, kernel_size=3, strides=(2, 2), activation='relu'),
+                tf.keras.layers.Conv2D(
+                    filters=64, kernel_size=3, strides=(2, 2), activation='relu'),
+                tf.keras.layers.Flatten(),
+                # No activation
+                tf.keras.layers.Dense(128 + 128),
+            ]
+        )
 
-saveLocation = 'saved/_dim{}.h5'.format(time.asctime(time.localtime()),latentDim)
+
+
+epochs = 10
+latent_dim = 128
+num_examples_to_generate = 16
+batchsize = 100
+test_train_ratio = 1 / 8  # is only used if test_size is null
+test_size = 200  # if test_size is null the test_train_ratio will be used
+data_source_dir = "Track1-RGB/Track1-RGB256x256"
+
+random_vector_for_generation = tf.random.normal(
+    shape=[num_examples_to_generate, latent_dim])
+
+keras.utils.plot_model(model, to_file="test_keras_plot_model.png", show_shapes=True)
+IPython.display.Image("test_keras_plot_model.png")
