947fb87db908b1c7599b94cbe40a4493bb8d9298
Hannes <hannes.staerk@gmail.com>
2019-08-21 18:35:23 +0200

    start experiments variational autoencoder architecture section

--- Kernel3avgPool16x16x32.py
+++ Kernel3avgPool16x16x32.py
@@ -1,10 +1,13 @@
-import errno
 import tensorflow as tf
 from keras import losses
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from matplotlib.image import BboxImage
+from matplotlib.transforms import Bbox, TransformedBbox
 import numpy as np
 import os
 import tifffile as tiff
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt, gridspec
 import time
 
 K = tf.keras.backend
@@ -44,8 +47,8 @@ variational_encoder = tf.keras.Model(
     inputs=[inputs], outputs=[codings_mean, codings_log_var, codings])
 
 decoder_inputs = tf.keras.layers.Input(shape=[latent_dim])
-x = tf.keras.layers.Dense(16 * 16 * 64)(decoder_inputs)
-x = tf.keras.layers.Reshape([16, 16, 64])(x)
+x = tf.keras.layers.Dense(16 * 16 * 32)(decoder_inputs)
+x = tf.keras.layers.Reshape([16, 16, 32])(x)
 x = tf.keras.layers.Conv2DTranspose(16, kernel_size=3, strides=2, padding="same", activation="selu")(x)
 x = tf.keras.layers.Conv2DTranspose(8, kernel_size=3, strides=2, padding="same", activation="selu")(x)
 outputs = tf.keras.layers.Conv2DTranspose(3, kernel_size=3, strides=2, padding="same", activation="sigmoid")(x)
@@ -59,7 +62,7 @@ latent_loss = -0.5 * K.sum(
     1 + codings_log_var - K.exp(codings_log_var) - K.square(codings_mean),
     axis=-1)
 variational_ae.add_loss(K.mean(latent_loss) / (128. * 128. * 3.))
-variational_ae.compile(loss=losses.mean_absolute_error, optimizer="rmsprop", metrics=['accuracy'])
+variational_ae.compile(loss=losses.mean_absolute_error, optimizer="rmsprop", metrics=['accuracy', 'mae'])
 
 
 def get_image_paths(dir):
@@ -99,8 +102,9 @@ if not os.path.exists("log"):
 
 
 def train():
-    last_epoch_mean = []
+    epoch_means = []
     for epoch in range(1, epochs + 1):
+        epoch_mean = []
         for iteration in range(train_iterations):
             first_index = iteration * batch_size
             batch = get_image_batch(training_paths, first_index, batch_size)
@@ -113,13 +117,32 @@ def train():
             f = open(log_file, "a")
             f.write(result)
             f.close()
-            if epoch == epochs:
-                last_epoch_mean.append(history)
-    last_epoch_mean = np.mean(last_epoch_mean, 0)
-    print(last_epoch_mean)
-    f = open(log_file, "a")
-    f.write(str(last_epoch_mean))
-    f.close()
+            epoch_mean.append(history)
+        epoch_mean = np.mean(epoch_mean, 0)
+        epoch_means.append(epoch_mean)
+        print("epoch mean: " + str(epoch_mean))
+        f = open(log_file, "a")
+        f.write("epoch mean: " + str(epoch_mean))
+        f.close()
+    epoch_means = np.array(epoch_means)
+    plt.clf()
+    plt.plot(epoch_means[:,0])
+    plt.ylim([0.06,0.2])
+    plt.ylabel("Loss")
+    plt.xlabel("Epochs")
+    plt.savefig(generation_path + "loss_graph" + file_tag, bbox_inches='tight', pad_inches=0)
+    plt.clf()
+    plt.plot(epoch_means[:, 1])
+    plt.ylim([0.3, 1])
+    plt.ylabel("Accuracy")
+    plt.xlabel("Epochs")
+    plt.savefig(generation_path + "accuracy_graph" + file_tag, bbox_inches='tight', pad_inches=0)
+    plt.clf()
+    plt.plot(epoch_means[:, 2])
+    plt.ylim([0.06,0.2])
+    plt.ylabel("Reconstruction Loss MAE")
+    plt.xlabel("Epochs")
+    plt.savefig(generation_path + "mae_graph" + file_tag, bbox_inches='tight', pad_inches=0)
 
 
 if not os.path.exists(generation_path):
@@ -129,7 +152,7 @@ if not os.path.exists(generation_path):
 def predictions_and_generations():
     pred_on = list()
     file_names = os.listdir("RGB-From-Track1128x128split8")
-    for filename in file_names[5:15]:
+    for filename in file_names[0:10]:
         pred_on.append(tiff.imread("RGB-From-Track1128x128split8/" + filename))
     pred_on = np.array(pred_on, dtype=np.float32)
     pred_on /= 255.
@@ -137,27 +160,20 @@ def predictions_and_generations():
     latent_log_var, latent_mean, latent_vars = variational_encoder.predict(pred_on)
     predictions = variational_decoder.predict(latent_vars)
 
-    for i in range(0, len(predictions)):
-        plt.clf()
-        plt.subplot(2, 3, 1)
-        plt.title('Latent Log Variance')
-        plt.plot(latent_log_var[i], 'o', markersize=1)
-        plt.subplot(2, 3, 2)
-        plt.title('Latent Mean')
-        plt.plot(latent_mean[i], 'o', markersize=1)
-        plt.subplot(2, 3, 3)
-        plt.title('Latent Sampled Variables')
-        plt.plot(latent_vars[i], 'o', markersize=1)
-        plt.subplot(2, 3, 4)
-        plt.title('Original Image')
-        plt.imshow(pred_on[i])
-        plt.subplot(2, 3, 5)
-        plt.title('Reconstructed Image')
-        plt.imshow(predictions[i])
+    number_predictions = len(predictions)
+    fig1, axs1 = plt.subplots(1, number_predictions, gridspec_kw={'wspace': 0, 'hspace': 0})
+    for i in range(0, number_predictions):
+        axs1[i].imshow(pred_on[i])
+        axs1[i].axis('off')
+    plt.tight_layout(pad=0)
+    plt.savefig(generation_path + "inputs" + file_tag, bbox_inches='tight', pad_inches=0)
 
-        plt.tight_layout()
-
-        plt.savefig(generation_path + "reconstruction" + str(i))
+    fig2, axs2 = plt.subplots(1, number_predictions, gridspec_kw={'wspace': 0, 'hspace': 0})
+    for i in range(0, number_predictions):
+        axs2[i].imshow(predictions[i])
+        axs2[i].axis('off')
+    plt.tight_layout(pad=0)
+    plt.savefig(generation_path + "reconstructions" + file_tag, bbox_inches='tight', pad_inches=0)
 
     codings = tf.random.normal(shape=[12, latent_dim])
     images_generated = variational_decoder.predict(codings, steps=1)
@@ -169,6 +185,69 @@ def predictions_and_generations():
         plt.imshow(images_generated[i])
         plt.savefig(generation_path + "generated" + str(i))
 
+
+def get_predominant_class_as_color(img):
+    flat = img.flatten()
+    most_common = np.bincount(flat).argmax()
+    if most_common == 2:  # ground
+        return 'gray'
+    elif most_common == 5:  # vegetation
+        return 'g'
+    elif most_common == 6:  # building
+        return 'r'
+    elif most_common == 9:  # water
+        return 'b'
+    elif most_common == 65:  # clutter
+        return 'y'
+
+
+def tsne_vis():
+    predominant_classes = []
+    locations = []
+    average_heights = []
+    images = []
+    file_names = os.listdir("RGB-From-Track1128x128split8")
+    print(len(file_names))
+    for filename in file_names[0:5000]:
+        image = tiff.imread("RGB-From-Track1128x128split8/" + filename)
+        cls = tiff.imread("Track1-Truth128x128split8/" + filename.replace("RGB", "CLS"))
+        predominant_classes.append(get_predominant_class_as_color(cls))
+        images.append(image)
+        if "OMA" in filename:
+            locations.append('.')
+        else:
+            locations.append('s')
+
+    images = np.array(images, dtype=np.float32)
+    images /= 255.
+
+    latent_log_var, latent_mean, latent_vars = variational_encoder.predict(images)
+    predictions = variational_decoder.predict(latent_vars)
+
+    print("t-SNE started with: " + str(latent_vars.shape))
+    embedded = TSNE(perplexity=15, n_iter=5000).fit_transform(latent_vars)
+    # embedded = PCA(n_components=2).fit_transform(latent_vars)
+    print("t-SNE finished")
+
+    plt.clf()
+    # plt.scatter(embedded[:, 0], embedded[:, 1], s=1, c=predominant_classes)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    for i in range(0, len(embedded)):
+        x = embedded[i, 0]
+        y = embedded[i, 1]
+        bb = Bbox.from_bounds(x, y, 1, 1)
+        bb2 = TransformedBbox(bb, ax.transData)
+        bbox_image = BboxImage(bb2,
+                               norm=None,
+                               origin=None,
+                               clip_on=False)
+        bbox_image.set_data(images[i])
+        ax.add_artist(bbox_image)
+
+    plt.show()
 
 start_time = time.time()
 train()
--- testScript.py
+++ testScript.py
@@ -1,9 +1,31 @@
-import tensorflow as tf
-import cv2
-from matplotlib import pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
-import sys
+import os
+import tifffile as tiff
 
-test = [1,2,3,4,5,6,7,8,9,10]
 
-print(test[5:10])
+t = np.arange(0.0, 2.0, 0.01)
+
+s1 = np.sin(2 * np.pi * t)
+s2 = np.exp(-t)
+s3 = s1 * s2
+
+
+pred_on = list()
+file_names = os.listdir("RGB-From-Track1128x128split8")
+for filename in file_names[0:5]:
+    pred_on.append(tiff.imread("RGB-From-Track1128x128split8/" + filename))
+pred_on = np.array(pred_on, dtype=np.float32)
+pred_on /= 255.
+
+fig, axs = plt.subplots(3, 10, sharex=True)
+# Remove horizontal space between axes
+fig.subplots_adjust(hspace=0)
+
+for i in range(0,3):
+    for j in range(0,10):
+        axs[i,j].imshow(pred_on[1])
+        axs[i,j].axis('off')
+
+
+plt.show()
