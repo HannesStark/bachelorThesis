e95bf11025ffe2609b68e5b9d312677e20d6aba7
Hannes <hannes.staerk@gmail.com>
2019-08-30 12:33:04 +0200

    finish latent space experiments

--- Kernel3adjusted16x16x32.py
+++ Kernel3adjusted16x16x32.py
@@ -13,7 +13,7 @@ import time
 
 K = tf.keras.backend
 
-latent_dim = 128
+latent_dim = 50
 file_tag = "16x16x32TRAINEDON178000" + "_dim" + str(latent_dim)
 # file_tag = os.path.splitext(os.path.basename(__file__))[0] + "_dim" + str(latent_dim)
 batch_size = 128
@@ -298,15 +298,15 @@ def tsne_vis(image_plot=False):
     plt.show()
 
 
-start_time = time.time()
-train()
-f = open(log_file, "a")
-f.write(str(time.time() - start_time))
-f.close()
-variational_ae.save_weights("./savedModels/" + file_tag + ".h5")
+#start_time = time.time()
+#train()
+#f = open(log_file, "a")
+#f.write(str(time.time() - start_time))
+#f.close()
+#variational_ae.save_weights("./savedModels/" + file_tag + ".h5")
 
-#variational_ae.load_weights("./savedModels/" + file_tag + ".h5")
+variational_ae.load_weights("./savedModels/" + file_tag + ".h5")
 
-# predictions_and_generations()
+predictions_and_generations()
 
 #tsne_vis(True)
