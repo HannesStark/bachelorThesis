d53ca12c316a18446296827f4d3baf35834be1ea
Hannes <hannes.staerk@gmail.com>
2019-08-30 16:03:10 +0200

    finish bachelorthesis for correction. Only figures from two architectures are missing now

--- Kernel3adjusted16x16x32.py
+++ Kernel3adjusted16x16x32.py
@@ -13,7 +13,7 @@ import time
 
 K = tf.keras.backend
 
-latent_dim = 50
+latent_dim = 128
 file_tag = "16x16x32TRAINEDON178000" + "_dim" + str(latent_dim)
 # file_tag = os.path.splitext(os.path.basename(__file__))[0] + "_dim" + str(latent_dim)
 batch_size = 128
